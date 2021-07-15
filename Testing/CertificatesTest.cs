078e82e1cf71265474bbf93d29540113851b773f
Takas0522 <t_o_19880522@outlook.com>
2021-07-15 20:40:31 +0900

    fix core 3.1

--- Testing/CertificatesTest.cs
+++ Testing/CertificatesTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using XadesNetLib.Utils.Certificates;
 using XadesNetLib.Utils.Certificates.Exceptions;
 
